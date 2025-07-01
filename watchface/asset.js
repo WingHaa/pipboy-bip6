07871f68e863c17c82e4ad6b392466c365aba550
WingHaa <quanlm24@gmail.com>
2025-07-01 11:06:20 +0700

    feat: refactor image

---
+++ watchface/asset.js
@@ -0,0 +1,94 @@
+import { assets } from "@zos/utils";
+
+export const img = assets("widgets");
+export const transparentButton = img("Empty.png");
+
+export const bar = [
+  img("0046.png"),
+  img("0047.png"),
+  img("0048.png"),
+  img("0049.png"),
+  img("0050.png"),
+  img("0051.png"),
+];
+
+export const numbers = [
+  img("0011.png"),
+  img("0012.png"),
+  img("0013.png"),
+  img("0014.png"),
+  img("0015.png"),
+  img("0016.png"),
+  img("0017.png"),
+  img("0018.png"),
+  img("0019.png"),
+  img("0020.png"),
+];
+
+export const weathers = [
+  img("0081.png"),
+  img("0082.png"),
+  img("0083.png"),
+  img("0084.png"),
+  img("0085.png"),
+  img("0086.png"),
+  img("0087.png"),
+  img("0088.png"),
+  img("0089.png"),
+  img("0090.png"),
+  img("0091.png"),
+  img("0092.png"),
+  img("0093.png"),
+  img("0094.png"),
+  img("0095.png"),
+  img("0096.png"),
+  img("0097.png"),
+  img("0098.png"),
+  img("0099.png"),
+  img("0100.png"),
+  img("0101.png"),
+  img("0102.png"),
+  img("0103.png"),
+  img("0104.png"),
+  img("0105.png"),
+  img("0106.png"),
+  img("0107.png"),
+  img("0108.png"),
+  img("0109.png"),
+];
+
+export const smallNumbers = [
+  img("0070.png"),
+  img("0071.png"),
+  img("0072.png"),
+  img("0073.png"),
+  img("0074.png"),
+  img("0075.png"),
+  img("0076.png"),
+  img("0077.png"),
+  img("0078.png"),
+  img("0079.png"),
+];
+
+export const hourNumbers = [
+  img("0001.png"),
+  img("0002.png"),
+  img("0003.png"),
+  img("0004.png"),
+  img("0005.png"),
+  img("0006.png"),
+  img("0007.png"),
+  img("0008.png"),
+  img("0009.png"),
+  img("0010.png"),
+];
+
+export const weekdays = [
+  img("0026.png"),
+  img("0027.png"),
+  img("0028.png"),
+  img("0029.png"),
+  img("0030.png"),
+  img("0031.png"),
+  img("0032.png"),
+];
